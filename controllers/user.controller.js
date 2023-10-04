1190c0faab606e6fddefc8bfd892fa7b7a7b37ce
Arushi Aggarwal <arushi3.14@outlook.com>
2023-10-04 14:30:28 -0400

    More Organization
    Pulled out all the user accessor code into separate files and folders to keep everything straight and clear.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,23 +1,10 @@
 // contains all methods responsible for responding to the different interactions a user can have with the site
 import User from "../models/user.js";
-import Connection from "../db/connection.js";
+import UserAccessor from "../db_accessor/user.accessor.js";
 
 export default class UserController {
     static async getAllUsers(req, res) {
-        /////////////////////////////////
-        /*   Connect to the database   */
-        /////////////////////////////////
-        await Connection.open("users");
-
-        const users = [];
-        try {
-            for await (const doc of User.find()) {
-                users.push(doc);
-            }
-        } catch (e) {
-            throw e;
-        }
-
+       const users = await UserAccessor.getAllUsers();
         res.json(users);
     }
 }
