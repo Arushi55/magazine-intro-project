888857a3becda38ac8a4849dfb4878f08e9a493c
Arushi Aggarwal <arushi3.14@outlook.com>
2023-10-30 20:01:47 -0400

    Phase 5
    Working on the front end - something in the sign_up.ejs is broken. Sign up page is rendering wrong.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,14 @@ import UserAccessor from "../db_accessor/user.accessor.js";
 export default class UserController {
     static async getAllUsers(req, res) {
        const users = await UserAccessor.getAllUsers();
-       console.log(users); 
-       res.render("index", {users: users});
+       res.render("index", { users: users });
+    }
+
+    static getLoginPage(req, res) {
+        res.render("login_page");
+    }
+
+    static getSignUpPage(req, res) {
+        res.render("sign_up");
     }
 }
--- routes/pages.route.js
+++ routes/pages.route.js
@@ -6,12 +6,16 @@ const router = express.Router();
 
 router.route("/").get(UserController.getAllUsers);
 
-router.route("/page2").get((req, res) => {
-    res.sendFile(path.resolve() + '/page2.html');
-});
-
 router.route('/css/index.css').get((req, res) => {
     res.sendFile(path.resolve() + "/css/index.css");
 });
+
+router.route("/css/style.css").get((req, res) => {
+    res.sendFile(path.resolve() + "/css/style.css");
+});
+
+router.route("/login-page").get(UserController.getLoginPage);
+
+router.route("/sign-up").get(UserController.getSignUpPage);
 
 export default router;
